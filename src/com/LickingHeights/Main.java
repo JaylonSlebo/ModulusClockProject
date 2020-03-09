b6f53b63adafd2fd0f7df604cad7c7494a9e3f44
Jaylon Slebo <jaylon.slebo.2023@student.lhschools.org>
2020-03-09 12:01:20 -0400

    userDataCaptured

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,37 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ModulusClock();
     }
 }
+
+    public static void ModulusClock(){
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+        System.out.println("Type in an amount of hours");
+        int  hours;
+        hours = keyboard.nextInt();
+        System.out.println("Type in an amount of minutes");
+        int minutes;
+        minutes = keyboard.nextInt();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
