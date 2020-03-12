4d4110d127b7478df2d5f6dda29a60fcffca1687
Jaylon Slebo <jaylon.slebo.2023@student.lhschools.org>
2020-03-12 11:56:56 -0400

    Ternary Operator Built For Hours

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,32 +6,33 @@ public class Main {
 
     public static void main(String[] args) {
         ModulusClock();
-    }
+    }{
 }
 
-    public static void ModulusClock(){
-        Scanner keyboard;
-        keyboard = new Scanner(System.in);
-        System.out.println("Type in an amount of hours");
-        int  hours;
-        hours = keyboard.nextInt();
-        System.out.println("Type in an amount of minutes");
+    public static void ModulusClock() {
         int minutes;
+        int hours;
+
+        Scanner keyboard;
+
+        keyboard = new Scanner(System.in);
+
+        System.out.println("Type in an amount of hours");
+        hours = keyboard.nextInt();
+
+
+
+
+        System.out.println("Type in an amount of minutes");
+
         minutes = keyboard.nextInt();
 
+        hours = minutes >= 60 ? hours+minutes/60 : hours;
+        System.out.println(minutes%60);
+        
 
+        System.out.println("The time is "+ hours +":"+ minutes);
 
-
-
-
-
-
-
-
-
-
-
-
-
+    }
 
     }
